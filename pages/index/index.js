afb0b70d26cda2465e1502f0639f6dd67e77ff84
adore <71892513@qq.com>
2018-03-15 18:17:08 +0800

    first

---
+++ pages/index/index.js
@@ -0,0 +1,82 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    // motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    hasProject: false
+    // canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  //事件处理函数
+  // bindViewTap: function() {
+  //   wx.navigateTo({
+  //     url: '../logs/logs'
+  //   })
+  // },
+  onLoad: function () {
+    var self = this
+    wx.getUserInfo({
+      success: function(res) {
+        // userInfo,nickName,avatarUrl,gender,province,city,country
+        self.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    })
+    wx.setTabBarItem({
+      index: 0,
+      text: 'text'
+      // iconPath: '/path/to/iconPath',
+      // selectedIconPath: '/path/to/selectedIconPath'
+    })
+    wx.setTabBarStyle({
+      color: '#FF0000',
+      selectedColor: '#00FF00',
+      backgroundColor: '#0000FF',
+      borderStyle: 'white'
+    })
+    wx.showTabBar({})
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      console.log('this.data.canIUse:' + this.data.canIUse)
+      // this.getUserInfo()
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        console.log('*************')
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function(e) {
+    console.log('*******getUserInfo*******')
+    // console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
